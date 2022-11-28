440294ef9e23f66dc99c75aa17ae97ff56868c5e
Tin Pritisanac <tin.priti@gmail.com>
2022-11-28 22:14:02 +0100

    Glavne funkcije

--- db_connector.py
+++ db_connector.py
@@ -1,1 +1,17 @@
 # kamen temeljac
+
+###### FUNKCIJE ZAPOSLENIKA ######
+def add_zaposlenik():
+    pass
+
+def edit_zaposlenik():
+    pass
+
+def delete_zaposlenik():
+    pass
+
+def print_all_zaposlenik():
+    pass
+
+def print_found_zaposlenik():
+    pass
