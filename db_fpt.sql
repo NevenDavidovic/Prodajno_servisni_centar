0a9eba0c1077c0dd880eb1dd43c03255d47885f7
Tin Pritišanac <tin.priti@gmail.com>
2023-01-03 15:36:28 +0100

    Dodani triggeri za popuste na racune

--- db_fpt.sql
+++ db_fpt.sql
@@ -23,6 +23,33 @@ DELIMITER ;
 -- SARA GOTOVA
 
 
+
+-- DARJAN FPO
+
+-- Napravi okidač koji za postojećeg klijenta (kupca) smanjuje cijenu novog vozila kojeg je kupio/la za 10%
+DELIMITER //
+CREATE TRIGGER popust_10
+BEFORE INSERT ON racun_prodaje
+FOR EACH ROW
+BEGIN
+declare br_klijenata integer;
+select count(id) into br_klijenata
+from racun_prodaje
+where id_klijent = new.id_klijent;
+if br_klijenata > 0 then
+set new.cijena = new.cijena - (new.cijena * 0.1);
+end if;
+END//
+DELIMITER ;
+
+-- INSERT INTO racun_prodaje VALUES (31, 4, 96, 1, 31, "2022-04-04 00:00:00", 121320.53957275549);
+
+-- select * from racun_prodaje;
+
+-- okidač radi, cijena vozila na računu je umanjena za 10%
+
+
+
 -- TIN FUNKCIJE, PROCEDURE I OKIDACI
 -- FUNKCIJE ZA ODREĐIVANJE CIJENE PRODANIH AUTOMOBILA
 ## Funkcija koja vraca namjmanju cijenu u tablici racun_prodaje
@@ -58,31 +85,41 @@ RETURN ((SELECT MAX(cijena) FROM racun_prodaje) - (SELECT MIN(cijena) FROM racun
 END//
 DELIMITER ;
 
-# TRIGGER koji daje klijentima koji su ujedno zaposlenici dodatan popust
-# TRIGGER koji daje dodatan popust ljudima iz ZG-a
--- TIN GOTOVO
-
--- DARJAN FPO
-
--- Napravi okidač koji za postojećeg klijenta (kupca) smanjuje cijenu novog vozila kojeg je kupio/la za 10%
+# TRIGGER koji daje klijentima koji su ujedno zaposlenici popust u visini od jedne njihove place
 DELIMITER //
-CREATE TRIGGER popust_10
+CREATE TRIGGER popust_za_zaposlenike_u_visini_jedne_place
 BEFORE INSERT ON racun_prodaje
 FOR EACH ROW
 BEGIN
-declare br_klijenata integer;
-select count(id) into br_klijenata
-from racun_prodaje
-where id_klijent = new.id_klijent;
-if br_klijenata > 0 then
-set new.cijena = new.cijena - (new.cijena * 0.1);
-end if;
+DECLARE br_zaposlenika INTEGER;
+DECLARE z_placa NUMERIC(8,2);
+DECLARE klijent_oib VARCHAR(20);
+
+-- određivanje OIB-a
+SELECT oib INTO klijent_oib
+FROM klijent WHERE id = new.id_klijent;
+
+-- prebrojavanje zaposlenika sa oibom klijenta
+SELECT count(*) INTO br_zaposlenika
+FROM zaposlenik 
+WHERE oib = klijent_oib;
+
+-- (vrijednost veca od 0 znaci klijent je ujedno i zaposlenik)
+IF br_zaposlenika > 0 THEN
+	-- dobivanje place zaposlenika
+	SELECT placa INTO z_placa
+	FROM zaposlenik
+    WHERE oib = klijent_oib; 
+    
+    -- ako je cijena umanjena za placu veca od nule oduzmi, inace stavi na nulu (sprijecavamo negativne vrijednosti cijene)
+    IF new.cijena - z_placa >= 0 THEN
+		SET new.cijena = new.cijena - z_placa;
+	ELSE SET new.cijena = 0;
+	END IF;
+END IF;
+
 END//
 DELIMITER ;
-
--- INSERT INTO racun_prodaje VALUES (31, 4, 96, 1, 31, "2022-04-04 00:00:00", 121320.53957275549);
-
--- select * from racun_prodaje;
-
--- okidač radi, cijena vozila na računu je umanjena za 10%
+# TRIGGER koji daje dodatan popust ljudima iz ZG-a
+-- TIN GOTOVO
 
